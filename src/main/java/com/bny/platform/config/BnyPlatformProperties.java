1a6990642fedbb469800e04efa3f68215e087b16
Chris Bunny <christopherjbunny@gmail.com>
2026-06-09 23:10:12 -0500

    Added lombok as dependency, converted YAML file to a ConfigurationProperties class

--- src/main/java/com/bny/platform/config/BnyPlatformProperties.java
+++ src/main/java/com/bny/platform/config/BnyPlatformProperties.java
@@ -1,4 +1,13 @@
 package com.bny.platform.config;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "bny.platform")
+@Data
 public class BnyPlatformProperties {
+
+    private String correlationHeader = "X-Correlation-Id";
+    private String healthPath = "/platform/health";
+    private boolean enabled = true;
 }
